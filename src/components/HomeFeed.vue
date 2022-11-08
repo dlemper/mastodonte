0d38407fe83846dd6ee1a855384424ae7f300f01
Daniel Lemper <ich@dlemper.de>
2022-11-08 19:09:49 +0100

    show public feed

--- src/components/HomeFeed.vue
+++ src/components/HomeFeed.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import {
   ChatBubbleLeftEllipsisIcon,
@@ -18,7 +19,7 @@ const oauthApi = mande(`${BASE_URL}/oauth`);
 
 const timelinesApi = mande(`${BASE_URL}/api/v1/timelines`);
 
-const client_name = "Mastodonte";
+/*const client_name = "Mastodonte";
 const redirectUri = `${location.origin}/settings/instances/add`
 const scopes = 'read write follow push'
 //const website = "";
@@ -41,31 +42,39 @@ const { access_token } = await oauthApi.post({
   code: route.params.code
 });
 
-defaults.headers.Authorization = `Bearer ${access_token}`;
+defaults.headers.Authorization = `Bearer ${access_token}`;*/
+
+const timeline = ref();
+console.log("mande timeline")
+timeline.value = await timelinesApi.get("public");
+console.log(timeline);
 </script>
 
 <template>
   <div class="mt-4">
     <h1 class="sr-only">Recent Toots</h1>
-<!--
+
     <ul role="list" class="space-y-4">
-      <li v-for="question in questions" :key="question.id" class="bg-white px-4 py-6 shadow sm:rounded-lg sm:p-6">
-        <article :aria-labelledby="'question-title-' + question.id">
+      <li v-for="entry in timeline" :key="entry.id" class="bg-white px-4 py-6 shadow sm:rounded-lg sm:p-6">
+        <article :aria-labelledby="'entry-title-' + entry.id">
           <div>
             <div class="flex space-x-3">
               <div class="flex-shrink-0">
-                <img class="h-10 w-10 rounded-full" :src="question.author.imageUrl" alt="" />
+                <img class="h-10 w-10 rounded-full" :src="entry.account.avatar" alt="" />
               </div>
+
               <div class="min-w-0 flex-1">
                 <p class="text-sm font-medium text-gray-900">
-                  <a :href="question.author.href" class="hover:underline">{{ question.author.name }}</a>
+                  <a :href="entry.account.url" class="hover:underline">{{ entry.account.display_name }}</a>
                 </p>
+
                 <p class="text-sm text-gray-500">
-                  <a :href="question.href" class="hover:underline">
-                    <time :datetime="question.datetime">{{ question.date }}</time>
+                  <a :href="entry.url" class="hover:underline">
+                    <time :datetime="entry.created_at">{{ entry.created_at }}</time>
                   </a>
                 </p>
               </div>
+
               <div class="flex flex-shrink-0 self-center">
                 <Menu as="div" class="relative inline-block text-left">
                   <div>
@@ -83,25 +92,27 @@ defaults.headers.Authorization = `Bearer ${access_token}`;
                       class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <div class="py-1">
                         <MenuItem v-slot="{ active }">
-                        <a href="#"
-                          :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'flex px-4 py-2 text-sm']">
-                          <StarIcon class="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-                          <span>Add to favorites</span>
-                        </a>
+                          <a href="#"
+                            :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'flex px-4 py-2 text-sm']">
+                            <StarIcon class="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+                            <span>Add to favorites</span>
+                          </a>
                         </MenuItem>
+
                         <MenuItem v-slot="{ active }">
-                        <a href="#"
-                          :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'flex px-4 py-2 text-sm']">
-                          <CodeBracketIcon class="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-                          <span>Embed</span>
-                        </a>
+                          <a href="#"
+                            :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'flex px-4 py-2 text-sm']">
+                            <CodeBracketIcon class="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+                            <span>Embed</span>
+                          </a>
                         </MenuItem>
+
                         <MenuItem v-slot="{ active }">
-                        <a href="#"
-                          :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'flex px-4 py-2 text-sm']">
-                          <FlagIcon class="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-                          <span>Report content</span>
-                        </a>
+                          <a href="#"
+                            :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'flex px-4 py-2 text-sm']">
+                            <FlagIcon class="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+                            <span>Report content</span>
+                          </a>
                         </MenuItem>
                       </div>
                     </MenuItems>
@@ -109,31 +120,33 @@ defaults.headers.Authorization = `Bearer ${access_token}`;
                 </Menu>
               </div>
             </div>
-            <h2 :id="'question-title-' + question.id" class="mt-4 text-base font-medium text-gray-900">{{
-            question.title }}</h2>
+            <!--<h2 :id="'question-title-' + question.id" class="mt-4 text-base font-medium text-gray-900">{{
+            question.title }}</h2>-->
           </div>
-          <div class="mt-2 space-y-4 text-sm text-gray-700" v-html="question.body" />
+
+          <div class="mt-2 space-y-4 text-sm text-gray-700 prose overflow-hidden" v-html="entry.content" />
+
           <div class="mt-6 flex justify-between space-x-8">
             <div class="flex space-x-6">
               <span class="inline-flex items-center text-sm">
                 <button type="button" class="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
                   <HandThumbUpIcon class="h-5 w-5" aria-hidden="true" />
-                  <span class="font-medium text-gray-900">{{ question.likes }}</span>
+                  <span class="font-medium text-gray-900">{{ entry.favourites_count }}</span>
                   <span class="sr-only">likes</span>
                 </button>
               </span>
               <span class="inline-flex items-center text-sm">
                 <button type="button" class="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
                   <ChatBubbleLeftEllipsisIcon class="h-5 w-5" aria-hidden="true" />
-                  <span class="font-medium text-gray-900">{{ question.replies }}</span>
+                  <span class="font-medium text-gray-900">{{ entry.replies_count }}</span>
                   <span class="sr-only">replies</span>
                 </button>
               </span>
               <span class="inline-flex items-center text-sm">
                 <button type="button" class="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
                   <EyeIcon class="h-5 w-5" aria-hidden="true" />
-                  <span class="font-medium text-gray-900">{{ question.views }}</span>
-                  <span class="sr-only">views</span>
+                  <span class="font-medium text-gray-900">{{ entry.reblogs_count }}</span>
+                  <span class="sr-only">reblogs</span>
                 </button>
               </span>
             </div>
@@ -149,7 +162,5 @@ defaults.headers.Authorization = `Bearer ${access_token}`;
         </article>
       </li>
     </ul>
-    
-  -->
   </div>
 </template>
