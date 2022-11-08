c470b97e5ef68d6ade3653cfa8f5aee7cdff6cb5
Daniel Lemper <ich@dlemper.de>
2022-11-08 20:10:36 +0100

    fix responsivesness a little

--- src/App.vue
+++ src/App.vue
@@ -21,18 +21,11 @@ const tabs = [
         
         <main class="lg:col-span-9 xl:col-span-6">
           <div class="px-4 sm:px-0">
-            <div class="sm:hidden">
-              <label for="question-tabs" class="sr-only">Select a tab</label>
-              <select id="question-tabs"
-                class="block w-full rounded-md border-gray-300 text-base font-medium text-gray-900 shadow-sm focus:border-rose-500 focus:ring-rose-500">
-                <option v-for="tab in tabs" :key="tab.name" :selected="tab.current">{{ tab.name }}</option>
-              </select>
-            </div>
-            <div class="hidden sm:block">
+            <div class="sm:block">
               <nav class="isolate flex divide-x divide-gray-200 rounded-lg shadow" aria-label="Tabs">
                 <a v-for="(tab, tabIdx) in tabs" :key="tab.name" :href="tab.href"
                   :aria-current="tab.current ? 'page' : undefined"
-                  :class="[tab.current ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700', tabIdx === 0 ? 'rounded-l-lg' : '', tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '', 'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10']">
+                  :class="[tab.current ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700', tabIdx === 0 ? 'rounded-l-lg' : '', tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '', 'group relative min-w-0 flex-1 overflow-hidden bg-white py-2 sm:py-4 px-2 lg:px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10']">
                   <span>{{ tab.name }}</span>
                   <span aria-hidden="true"
                     :class="[tab.current ? 'bg-rose-500' : 'bg-transparent', 'absolute inset-x-0 bottom-0 h-0.5']" />
