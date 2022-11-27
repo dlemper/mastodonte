c1eaaf71a802ded4b7e146c6d8a82d45e748b5e9
Daniel Lemper <ich@dlemper.de>
2022-11-27 11:53:53 +0100

    add showing reblogs correctly

--- src/components/HomeFeed.vue
+++ src/components/HomeFeed.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref } from 'vue';
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { ref } from "vue";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
 import {
   ChatBubbleLeftEllipsisIcon,
   CodeBracketIcon,
@@ -10,47 +10,18 @@ import {
   HandThumbUpIcon,
   ShareIcon,
   StarIcon,
-} from '@heroicons/vue/20/solid'
-import { mande, defaults } from "mande";
-import formatDistanceToNow from 'date-fns/formatDistanceToNow';
-import parseJSON from 'date-fns/parseJSON';
-import { de } from 'date-fns/locale'
+} from "@heroicons/vue/20/solid";
+import { ArrowPathIcon } from "@heroicons/vue/20/solid";
+import { mande } from "mande";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import parseJSON from "date-fns/parseJSON";
+import { de } from "date-fns/locale";
 
-const BASE_URL = 'https://norden.social';
-const appsApi = mande(`${BASE_URL}/api/v1/apps`);
-const oauthApi = mande(`${BASE_URL}/oauth`);
-
+const BASE_URL = localStorage.getItem("BASE_URL") ?? "";
 const timelinesApi = mande(`${BASE_URL}/api/v1/timelines`);
 
-/*const client_name = "Mastodonte";
-const redirectUri = `${location.origin}/settings/instances/add`
-const scopes = 'read write follow push'
-//const website = "";
-
-const instanceInfo = await appsApi.post({
-  client_name,
-  redirect_uris: redirectUri,
-  scopes,
-  //website
-});
-setTimeout(() => {
-  document.location.href = `${BASE_URL}/oauth/authorize?client_id=${encodeURIComponent(instanceInfo.client_id)}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scopes=${encodeURIComponent(scopes)}`
-}, 200);
-
-const { access_token } = await oauthApi.post({
-  client_id: instanceInfo.client_id,
-  client_secret: instanceInfo.client_secret,
-  redirect_uri: redirectUri,
-  grant_type: 'authorization_code',
-  code: route.params.code
-});
-
-defaults.headers.Authorization = `Bearer ${access_token}`;*/
-
 const timeline = ref();
-console.log("mande timeline")
-timeline.value = await timelinesApi.get("public");
-console.log(timeline);
+timeline.value = await timelinesApi.get("home");
 </script>
 
 <template>
@@ -58,22 +29,72 @@ console.log(timeline);
     <h1 class="sr-only">Recent Toots</h1>
 
     <ul role="list" class="space-y-4">
-      <li v-for="entry in timeline" :key="entry.id" class="bg-white px-4 py-6 shadow sm:rounded-lg sm:p-6">
+      <li
+        v-for="entry in timeline"
+        :key="entry.id"
+        class="bg-white px-4 py-6 shadow sm:rounded-lg sm:p-6"
+      >
         <article :aria-labelledby="'entry-title-' + entry.id">
+          <span class="pl-12 text-gray-400 text-xs" v-if="entry.reblog">
+            <ArrowPathIcon class="w-3 h-3 inline"></ArrowPathIcon>
+            {{ entry.account.display_name }}
+          </span>
+
           <div>
             <div class="flex space-x-3">
               <div class="flex-shrink-0">
-                <img class="h-10 w-10 rounded-full" :src="entry.account.avatar" alt="" />
+                <img
+                  class="h-10 w-10 rounded-full"
+                  :src="
+                    entry.reblog
+                      ? entry.reblog.account.avatar
+                      : entry.account.avatar
+                  "
+                  alt=""
+                />
               </div>
 
               <div class="min-w-0 flex-1">
                 <p class="text-sm font-medium text-gray-900">
-                  <a :href="entry.account.url" class="hover:underline">{{ entry.account.display_name }}</a>
+                  <a
+                    :href="
+                      entry.reblog
+                        ? entry.reblog.account.url
+                        : entry.account.url
+                    "
+                    class="hover:underline"
+                    >{{
+                      entry.reblog
+                        ? entry.reblog.account.display_name
+                        : entry.account.display_name
+                    }}</a
+                  >
                 </p>
 
                 <p class="text-sm text-gray-500">
-                  <a :href="entry.url" class="hover:underline">
-                    <time :datetime="entry.created_at">{{ formatDistanceToNow(parseJSON(entry.created_at), { locale: de }) }}</time>
+                  <a
+                    :href="entry.reblog ? entry.reblog.url : entry.url"
+                    class="hover:underline"
+                  >
+                    <time
+                      :datetime="
+                        entry.reblog
+                          ? entry.reblog.created_at
+                          : entry.created_at
+                      "
+                      >{{
+                        formatDistanceToNow(
+                          parseJSON(
+                            entry.reblog
+                              ? entry.reblog.created_at
+                              : entry.created_at
+                          ),
+                          {
+                            locale: de,
+                          }
+                        )
+                      }}</time
+                    >
                   </a>
                 </p>
               </div>
@@ -81,39 +102,79 @@ console.log(timeline);
               <div class="flex flex-shrink-0 self-center">
                 <Menu as="div" class="relative inline-block text-left">
                   <div>
-                    <MenuButton class="-m-2 flex items-center rounded-full p-2 text-gray-400 hover:text-gray-600">
+                    <MenuButton
+                      class="-m-2 flex items-center rounded-full p-2 text-gray-400 hover:text-gray-600"
+                    >
                       <span class="sr-only">Open options</span>
-                      <EllipsisVerticalIcon class="h-5 w-5" aria-hidden="true" />
+                      <EllipsisVerticalIcon
+                        class="h-5 w-5"
+                        aria-hidden="true"
+                      />
                     </MenuButton>
                   </div>
-  
-                  <transition enter-active-class="transition ease-out duration-100"
-                    enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
-                    leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100"
-                    leave-to-class="transform opacity-0 scale-95">
+
+                  <transition
+                    enter-active-class="transition ease-out duration-100"
+                    enter-from-class="transform opacity-0 scale-95"
+                    enter-to-class="transform opacity-100 scale-100"
+                    leave-active-class="transition ease-in duration-75"
+                    leave-from-class="transform opacity-100 scale-100"
+                    leave-to-class="transform opacity-0 scale-95"
+                  >
                     <MenuItems
-                      class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    >
                       <div class="py-1">
                         <MenuItem v-slot="{ active }">
-                          <a href="#"
-                            :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'flex px-4 py-2 text-sm']">
-                            <StarIcon class="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+                          <a
+                            href="#"
+                            :class="[
+                              active
+                                ? 'bg-gray-100 text-gray-900'
+                                : 'text-gray-700',
+                              'flex px-4 py-2 text-sm',
+                            ]"
+                          >
+                            <StarIcon
+                              class="mr-3 h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
                             <span>Add to favorites</span>
                           </a>
                         </MenuItem>
 
                         <MenuItem v-slot="{ active }">
-                          <a href="#"
-                            :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'flex px-4 py-2 text-sm']">
-                            <CodeBracketIcon class="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+                          <a
+                            href="#"
+                            :class="[
+                              active
+                                ? 'bg-gray-100 text-gray-900'
+                                : 'text-gray-700',
+                              'flex px-4 py-2 text-sm',
+                            ]"
+                          >
+                            <CodeBracketIcon
+                              class="mr-3 h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
                             <span>Embed</span>
                           </a>
                         </MenuItem>
 
                         <MenuItem v-slot="{ active }">
-                          <a href="#"
-                            :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'flex px-4 py-2 text-sm']">
-                            <FlagIcon class="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+                          <a
+                            href="#"
+                            :class="[
+                              active
+                                ? 'bg-gray-100 text-gray-900'
+                                : 'text-gray-700',
+                              'flex px-4 py-2 text-sm',
+                            ]"
+                          >
+                            <FlagIcon
+                              class="mr-3 h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
                             <span>Report content</span>
                           </a>
                         </MenuItem>
@@ -127,35 +188,65 @@ console.log(timeline);
             question.title }}</h2>-->
           </div>
 
-          <div class="mt-2 space-y-4 text-sm text-gray-700 prose overflow-hidden" v-html="entry.content" />
+          <div
+            class="mt-2 space-y-4 text-sm text-gray-700 prose overflow-hidden"
+            v-html="entry.reblog ? entry.reblog.content : entry.content"
+          />
 
           <div class="mt-6 flex justify-between space-x-8">
             <div class="flex space-x-6">
               <span class="inline-flex items-center text-sm">
-                <button type="button" class="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
+                <button
+                  type="button"
+                  class="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
+                >
                   <HandThumbUpIcon class="h-5 w-5" aria-hidden="true" />
-                  <span class="font-medium text-gray-900">{{ entry.favourites_count }}</span>
+                  <span class="font-medium text-gray-900">{{
+                    entry.reblog
+                      ? entry.reblog.favourites_count
+                      : entry.favourites_count
+                  }}</span>
                   <span class="sr-only">likes</span>
                 </button>
               </span>
               <span class="inline-flex items-center text-sm">
-                <button type="button" class="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
-                  <ChatBubbleLeftEllipsisIcon class="h-5 w-5" aria-hidden="true" />
-                  <span class="font-medium text-gray-900">{{ entry.replies_count }}</span>
+                <button
+                  type="button"
+                  class="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
+                >
+                  <ChatBubbleLeftEllipsisIcon
+                    class="h-5 w-5"
+                    aria-hidden="true"
+                  />
+                  <span class="font-medium text-gray-900">{{
+                    entry.reblog
+                      ? entry.reblog.replies_count
+                      : entry.replies_count
+                  }}</span>
                   <span class="sr-only">replies</span>
                 </button>
               </span>
               <span class="inline-flex items-center text-sm">
-                <button type="button" class="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
+                <button
+                  type="button"
+                  class="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
+                >
                   <EyeIcon class="h-5 w-5" aria-hidden="true" />
-                  <span class="font-medium text-gray-900">{{ entry.reblogs_count }}</span>
+                  <span class="font-medium text-gray-900">{{
+                    entry.reblog
+                      ? entry.reblog.reblogs_count
+                      : entry.reblogs_count
+                  }}</span>
                   <span class="sr-only">reblogs</span>
                 </button>
               </span>
             </div>
             <div class="flex text-sm">
               <span class="inline-flex items-center text-sm">
-                <button type="button" class="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
+                <button
+                  type="button"
+                  class="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
+                >
                   <ShareIcon class="h-5 w-5" aria-hidden="true" />
                   <span class="font-medium text-gray-900">Share</span>
                 </button>
